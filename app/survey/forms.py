ae32b6a8343cd8f32f382b3dd3ead10a745d15e0
Jeff Abrahamson <jeff@p27.eu>
2020-02-05 13:31:54 +0100

    Add survey editor
    Part of #22.

--- app/survey/forms.py
+++ app/survey/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, HiddenField
+from wtforms import StringField, TextField, IntegerField, BooleanField, HiddenField
+from wtforms.widgets import TextArea
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from app.models import Survey
 
@@ -9,3 +10,10 @@ class SurveyForm(FlaskForm):
     name = StringField('Nom du sondage', validators=[DataRequired()])
     description = StringField('Description du sondage')
 
+class SurveyQuestionForm(FlaskForm):
+    id = HiddenField('id')
+    survey_id = HiddenField('survey_id')
+    question_number = StringField('Numéro')
+    sort_index = IntegerField('Position de trie')
+    question_title = StringField('Titre', validators=[DataRequired()])
+    question_text = StringField('Titre', widget=TextArea(), validators=[DataRequired()])
--- app/survey/routes.py
+++ app/survey/routes.py
@@ -4,10 +4,11 @@ from werkzeug.urls import url_parse
 from flask import current_app as app
 from flask_login import login_user, logout_user, current_user, login_required
 from app import db
-from app.models import Survey
-from app.survey.forms import SurveyForm
+from app.models import Survey, SurveyQuestion
+from app.survey.forms import SurveyForm, SurveyQuestionForm
 from app.survey import bp
 from app.survey.survey_v1 import do_municipales_responses_v1
+import sys
 
 @bp.route('/D/<tag>/municipales', defaults={'seed': None})
 @bp.route('/F/<tag>/<seed>/municipales')
@@ -50,45 +51,36 @@ def survey_edit(tag, seed):
     """
     # check_admin('survey')
     form = SurveyForm()
+    new_question_form = SurveyQuestionForm()
     survey = None
     survey_id = request.args.get('survey_id', None)
-    print('#### ', request.method, ' ####')
     if request.method == 'POST':
-        print('#### POST ####')
         if form.validate_on_submit():
-            print('#### validated ####')
             # The user may have changed the survey name, so first
             # search based on the name.
             survey = Survey.query.filter_by(name=form.name.data).one_or_none()
             if survey is None and survey_id is not None:
-                print('#### no survey, yes survey_id ####', survey_id)
                 # If we didn't find the survey by name but we have a
                 # survey_id, then we should use that.
                 survey = Survey.query.filter_by(id=survey_id).one_or_none()
             if survey is None:
-                print('#### no survey ####')
                 # If we still don't have a survey, then this is a new
                 # survey, so we must start by creating it.
-                print('#### New ####')
                 survey = Survey(name=form.name.data,
                                 description=form.description.data)
                 db.session.add(survey)
             else:
-                print('#### yes survey ####')
                 survey.id = form.id.data
                 survey.name = form.name.data
                 survey.description = form.description.data
             try:
-                print('#### Save ####')
                 db.session.commit()
-                print('#### Committed ####')
             except:
                 # If name already exists, for example.
                 # Even though we're editing, the user can change the name,
                 # and changing to existing isn't permitted.
                 #### How do I handle this?
                 #### I want to re-present the form with explanation message.
-                print('#### Exception ####')
                 db.session.rollback()
         else:
             # Problem with posted survey.
@@ -97,13 +89,13 @@ def survey_edit(tag, seed):
         return render_template('survey/mod_survey.html',
                                tag=tag, seed=seed,
                                form=form,
+                               new_question_form=new_question_form,
                                survey=survey,
                                questions=survey.questions)
 
     if survey_id is None:
-        print('#### survey_id is still none ####')
+        app.logger.warning('#### survey_id is still none in survey_edit() ####')
         render_template(''), 404
-    print('#### survey_id is not none ####', survey_id)
     survey = Survey.query.filter_by(id=survey_id).first_or_404()
     form.id.data = survey_id
     form.name.data = survey.name
@@ -111,5 +103,69 @@ def survey_edit(tag, seed):
     return render_template('survey/mod_survey.html',
                            tag=tag, seed=seed,
                            form=form,
+                           new_question_form=new_question_form,
                            survey=survey,
                            questions=survey.questions)
+
+@login_required
+@bp.route('/F/<tag>/<seed>/edit/question', methods=['GET', 'POST'])
+def question_edit(tag, seed):
+    """Create or modify a question.
+    """
+    # check_admin('survey')
+    print('#### question_edit ####')
+    question_form = SurveyQuestionForm()
+    question_id = request.args.get('question_id', None)
+    survey_id = request.args.get('survey_id', question_form.survey_id.data)
+    question = SurveyQuestion.query.filter_by(id=question_id).one_or_none()
+    if request.method == 'POST':
+        if question_form.validate_on_submit():
+            print('#### 1 ####')
+            if question is None:
+                print('#### question is none ####')
+                question = SurveyQuestion(
+                    survey_id=survey_id,
+                    question_number=question_form.question_number.data,
+                    sort_index=question_form.sort_index.data,
+                    question_title=question_form.question_title.data,
+                    question_text=question_form.question_text.data)
+                db.session.add(question)
+            else:
+                print('#### question is not null ####')
+                question.survey_id=survey_id
+                question.question_number=question_form.question_number.data
+                question.sort_index=question_form.sort_index.data
+                question.question_title=question_form.question_title.data
+                question.question_text=question_form.question_text.data
+            try:
+                db.session.commit()
+                print('#### commit ####')
+                return redirect(url_for('survey.survey_edit',
+                                        tag=tag, seed=seed,
+                                        survey_id=survey_id))
+            except:
+                print(sys.exc_info()[0])
+                print(sys.exc_info())
+                db.session.rollback()
+                print('#### rollback ####')
+        else:
+            return render_template('survey/mod_question.html',
+                                   tag=tag, seed=seed,
+                                   survey_id=survey_id,
+                                   question=question,
+                                   form=question_form)
+    # GET
+    app.logger.info(question_id)
+    question = SurveyQuestion.query.filter_by(id=question_id).one_or_none()
+    app.logger.info(question)
+    question_form.id.data = question.id
+    question_form.survey_id.data = question.survey_id
+    question_form.question_number.data = question.question_number
+    question_form.sort_index.data = question.sort_index
+    question_form.question_title.data = question.question_title
+    question_form.question_text.data = question.question_text
+    return render_template('survey/mod_question.html',
+                           tag=tag, seed=seed,
+                           question=question,
+                           survey_id=survey_id,
+                           form=question_form)
